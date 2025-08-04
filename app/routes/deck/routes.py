dcf4ed48478ff96e0ad99abfb36f356961bce410
walidkhamees <walid.khamees1973@gmail.com>
2025-08-04 16:38:17 +0300

    Feat: adds delete all deck functionality

--- app/routes/deck/routes.py
+++ app/routes/deck/routes.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, render_template, request
 
+from app.services.deck import create_deck, delete_all_decks, get_decks, delete_all_decks
+
 deck_bp = Blueprint("deck_bp", __name__, url_prefix="/deck")
 
 @deck_bp.route("/", methods=["GET"])
-def index():
-    return render_template("deck.html")
-
+def index_route():
+    decks = get_decks()
+    return render_template("deck.html", decks=decks)
 
 @deck_bp.route("/", methods=["POST"])
-def create_deck():
-    # put a confirm message here
+def create_deck_route():
     deck_name = request.form.get("name", "").strip()
-    return deck_name
+    message = create_deck(deck_name)
+    return message
+
+@deck_bp.route("/", methods=["DELETE"])
+def delete_all_decks_route():
+    delete_all_decks()
+    return ""
--- app/services/deck.py
+++ app/services/deck.py
@@ -1,9 +1,49 @@
 import os
-
-
+import re
 
 def get_decks():
     decks = []
     for file in os.listdir("app/decks"):
         decks.append(file.replace(".txt", ""))
     return decks
+
+def create_deck(deck_name):
+    cleaned_deck_name = re.sub(r"[[<>/?\\,:*| ]", "", deck_name)
+
+    if cleaned_deck_name != deck_name:
+        return "Deck name cannot contain special characters"
+
+    if deck_name == "":
+        return "Deck name cannot be empty"
+
+    deck_file = open(f"app/decks/{deck_name}.txt", "w")
+    deck_file.close()
+    return f"Deck {deck_name} created successfully"
+
+def get_deck(deck_name):
+    deck = {
+        "name": deck_name,
+        "cards": {
+
+        }
+    }
+    deck_file = open(f"app/decks/{deck_name}.txt", "r")
+    cards = deck_file.read().split("\n")
+    deck_file.close()
+    for card in cards:
+        card_splited = card.split("`")
+
+        card_id = card_splited[0]
+        card = {
+            "question": card_splited[1],
+            "answer": card_splited[2],
+            "last_answered": card_splited[3],
+            "correct": card_splited[4],
+        }
+        deck["cards"][card_id] = card
+
+    return deck
+
+def delete_all_decks():
+    for file in os.listdir("app/decks"):
+        os.remove(f"app/decks/{file}")
