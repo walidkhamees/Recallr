158db1af9539f2745599977ded1e9ff5a8b867a5
walidkhamees <walid.khamees1973@gmail.com>
2025-08-04 14:22:25 +0300

    style: styles the create deck form

--- app/routes/deck/routes.py
+++ app/routes/deck/routes.py
@@ -9,5 +9,6 @@ def index():
 
 @deck_bp.route("/", methods=["POST"])
 def create_deck():
+    # put a confirm message here
     deck_name = request.form.get("name", "").strip()
     return deck_name
